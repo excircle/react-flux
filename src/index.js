9817fa90edc356d1e3a15c0c5f382b72762d352e
excircle <unixdisciple.us@gmail.com>
2022-05-01 11:57:49 -0700

    Updated DOM settings to comply with react 18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 
-render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
