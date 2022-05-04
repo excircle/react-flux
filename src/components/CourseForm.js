28601d0a8116fa838bb6069fb4e0a51d0ee454ca
excircle <unixdisciple.us@gmail.com>
2022-05-03 17:54:28 -0700

    CourseForm.js is now using smart components

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,52 +1,33 @@
 import React from "react";
+import TextInput from "./common/TextInput";
+import SelectInput from "./common/SelectInput";
 
 function CourseForm(props) {
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <div className="field">
-          <input
-            id="title"
-            type="text"
-            onChange={props.onChange}
-            name="title"
-            className="form-control"
-            value={props.course.title}
-          />
-        </div>
-      </div>
+      <TextInput
+        id="title"
+        label="Title"
+        onChange={props.onChange}
+        name="title"
+        value={props.course.title}
+      />
 
-      <div className="form-group">
-        <label htmlFor="author">Author</label>
-        <div className="field">
-          <select
-            id="author"
-            name="authorId"
-            onChange={props.onChange}
-            value={props.course.authorId || ""}
-            className="form-control"
-          >
-            <option value="" />
-            <option value="1">Cory House</option>
-            <option value="2">Scott Allen</option>
-          </select>
-        </div>
-      </div>
+      <SelectInput
+        id="author"
+        name="authorId"
+        onChange={props.onChange}
+        value={props.course.authorId || ""}
+        className="form-control"
+      />
 
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <div className="field">
-          <input
-            type="text"
-            id="category"
-            name="category"
-            onChange={props.onChange}
-            className="form-control"
-            value={props.course.category}
-          />
-        </div>
-      </div>
+      <TextInput
+        id="category"
+        name="category"
+        label="Category"
+        onChange={props.onChange}
+        value={props.course.category}
+      />
 
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
